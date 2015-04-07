285ddd6274806a19513e281a7783fd7abd160eaf
mike <mike@guidebook.com>
2015-04-06 18:00:37 -0700

    hacky shit so i can subclass this stupid thing in swift

--- TGCameraViewController/Control/TGCameraNavigationController.h
+++ TGCameraViewController/Control/TGCameraNavigationController.h
@@ -28,24 +28,23 @@
 
 @interface TGCameraNavigationController : UINavigationController
 
-+ (instancetype)new __attribute__
-((unavailable("[+new] is not allowed, use [+newWithCameraDelegate:]")));
+// fucking shit fuck you swift fuck.  Have to comment these out otherwise I cant subclass (cz)
 
-- (instancetype) init __attribute__
-((unavailable("[-init] is not allowed, use [+newWithCameraDelegate:]")));
+//+ (instancetype)new;
+//
+//- (instancetype) init;
+//
+//- (id)initWithCoder:(NSCoder *)aDecoder;
 
-- (id)initWithCoder:(NSCoder *)aDecoder __attribute__
-((unavailable("[-initWithCoder:] is not allowed, use [+newWithCameraDelegate:]")));
+//- (instancetype)initWithNavigationBarClass:(Class)navigationBarClass toolbarClass:(Class)toolbarClass __attribute__
+//((unavailable("[-initWithNavigationBarClass:toolbarClass:] is not allowed, use [+newWithCameraDelegate:]")));
+//
+//- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil __attribute__
+//((unavailable("[-initWithNibName:bundle:] is not allowed, use [+newWithCameraDelegate:]")));
+//
+//- (instancetype)initWithRootViewController:(UIViewController *)rootViewController __attribute__
+//((unavailable("[-initWithRootViewController:] is not allowed, use [+newWithCameraDelegate:]")));
 
-- (instancetype)initWithNavigationBarClass:(Class)navigationBarClass toolbarClass:(Class)toolbarClass __attribute__
-((unavailable("[-initWithNavigationBarClass:toolbarClass:] is not allowed, use [+newWithCameraDelegate:]")));
-
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil __attribute__
-((unavailable("[-initWithNibName:bundle:] is not allowed, use [+newWithCameraDelegate:]")));
-
-- (instancetype)initWithRootViewController:(UIViewController *)rootViewController __attribute__
-((unavailable("[-initWithRootViewController:] is not allowed, use [+newWithCameraDelegate:]")));
-
-+ (instancetype)newWithCameraDelegate:(id<TGCameraDelegate>)delegate;
++ (instancetype)newWithCameraDelegate:(id<TGCameraDelegate>)delegate ;
 
 @end
