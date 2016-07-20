b7866af22ee0dcc29c1cb9cbc99fce87cb6b5da2
Mario Cecchi <macecchi@gmail.com>
2016-07-20 13:23:51 -0300

    ✨ Add Swift example project

---
+++ ExampleSwift/TGInitialViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  ExampleSwift
+//
+//  Created by Mario Cecchi on 7/20/16.
+//  Copyright © 2016 Tudo Gostoso Internet. All rights reserved.
+//
+
+import UIKit
+
+class TGInitialViewController: UIViewController, TGCameraDelegate {
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        TGCamera.setOption(kTGCameraOptionSaveImageToAlbum, value: true)
+        
+        photoView.clipsToBounds = true
+        
+        let clearButton = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action:#selector(clearTapped))
+        navigationItem.rightBarButtonItem = clearButton
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    // MARK: TGCameraDelegate - Required methods
+    
+    func cameraDidCancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func cameraDidTakePhoto(image: UIImage!) {
+        photoView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func cameraDidSelectAlbumPhoto(image: UIImage!) {
+        photoView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    // MARK: TGCameraDelegate - Optional methods
+    
+    func cameraWillTakePhoto() {
+        print("cameraWillTakePhoto")
+    }
+    
+    func cameraDidSavePhotoAtPath(assetURL: NSURL!) {
+        print("cameraDidSavePhotoAtPath: \(assetURL)")
+    }
+    
+    func cameraDidSavePhotoWithError(error: NSError!) {
+        print("cameraDidSavePhotoWithError \(error)")
+    }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func takePhotoTapped() {
+        let navigationController = TGCameraNavigationController.newWithCameraDelegate(self)
+        presentViewController(navigationController, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: Private methods
+    
+    func clearTapped() {
+        photoView.image = nil
+    }
+}
+
